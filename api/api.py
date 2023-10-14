09eb4e5fd3e21a1bed8fe1d8edb1888c006496ac
yarosshest <yarosshest@gmail.com>
2023-10-14 23:27:00 +0300

    parsing done

--- api/api.py
+++ api/api.py
@@ -38,5 +38,15 @@ async def add_post_post(latitude: float, longitude: float, db: Db = Depends(db_i
     return Office(latitude=point[0],longitude=point[1])
 
 
+@app.post("/recom_office",
+        responses={
+            200: {"model": Office, "description": "Successful Response"},
+        })
+async def add_post_post(latitude: float, longitude: float, req: Req, db: Db = Depends(db_ins)) ->Office:
+    point = await db.get_near_one(latitude, longitude)
+
+    return Office(latitude=point[0],longitude=point[1])
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8031)
